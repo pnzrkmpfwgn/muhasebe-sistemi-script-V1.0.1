8adbac117e296c81c4e4b204f0f9ef98f7f49ea5
pnzrkmpfwgn <kurtknispel9@gmail.com>
2021-09-08 12:45:32 +0300

    Finilized the system

--- muhasebe-sistemi-v1.0.1.js
+++ muhasebe-sistemi-v1.0.1.js
@@ -8,9 +8,9 @@
 function merkezTl(islemDurumu, sube, islem, miktar) {
   var toplam = 0;
   var tarih = SpreadsheetApp.getActiveSheet().getRange("G2").getDisplayValue();
-  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D9:D").getDisplayValues();
+  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D6:D").getDisplayValues();
   var ss = SpreadsheetApp.getActiveSheet();
-  var sonSatir = ss.getLastRow() - 9;
+  var sonSatir = ss.getLastRow();
 
   for (let i = 0; i <= sonSatir; i++) {
     if (
@@ -31,9 +31,9 @@ function merkezTl(islemDurumu, sube, islem, miktar) {
 function merkezDolar(islemDurumu,sube,islem,miktar){
    var toplam = 0;
   var tarih = SpreadsheetApp.getActiveSheet().getRange("G2").getDisplayValue();
-  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D9:D").getDisplayValues();
+  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D6:D").getDisplayValues();
   var ss = SpreadsheetApp.getActiveSheet();
-  var sonSatir = ss.getLastRow() - 9;
+  var sonSatir = ss.getLastRow() ;
 
   for (let i = 0; i <= sonSatir; i++) {
     if (
@@ -54,9 +54,9 @@ function merkezDolar(islemDurumu,sube,islem,miktar){
 function merkezEuro(islemDurumu,sube,islem,miktar){
    var toplam = 0;
   var tarih = SpreadsheetApp.getActiveSheet().getRange("G2").getDisplayValue();
-  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D9:D").getDisplayValues();
+  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D6:D").getDisplayValues();
   var ss = SpreadsheetApp.getActiveSheet();
-  var sonSatir = ss.getLastRow() - 9;
+  var sonSatir = ss.getLastRow();
 
   for (let i = 0; i <= sonSatir; i++) {
     if (
@@ -77,9 +77,9 @@ function merkezEuro(islemDurumu,sube,islem,miktar){
 function merkezSterlin(islemDurumu,sube,islem,miktar){
    var toplam = 0;
   var tarih = SpreadsheetApp.getActiveSheet().getRange("G2").getDisplayValue();
-  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D9:D").getDisplayValues();
+  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D6:D").getDisplayValues();
   var ss = SpreadsheetApp.getActiveSheet();
-  var sonSatir = ss.getLastRow() - 9;
+  var sonSatir = ss.getLastRow();
 
   for (let i = 0; i <= sonSatir; i++) {
     if (
@@ -100,9 +100,9 @@ function merkezSterlin(islemDurumu,sube,islem,miktar){
 function lefkosaTl(islemDurumu,sube,islem,miktar){
    var toplam = 0;
   var tarih = SpreadsheetApp.getActiveSheet().getRange("G2").getDisplayValue();
-  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D9:D").getDisplayValues();
+  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D6:D").getDisplayValues();
   var ss = SpreadsheetApp.getActiveSheet();
-  var sonSatir = ss.getLastRow() - 9;
+  var sonSatir = ss.getLastRow();
 
   for (let i = 0; i <= sonSatir; i++) {
     if (
@@ -123,9 +123,9 @@ function lefkosaTl(islemDurumu,sube,islem,miktar){
 function lefkosaDolar(islemDurumu,sube,islem,miktar){
    var toplam = 0;
   var tarih = SpreadsheetApp.getActiveSheet().getRange("G2").getDisplayValue();
-  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D9:D").getDisplayValues();
+  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D6:D").getDisplayValues();
   var ss = SpreadsheetApp.getActiveSheet();
-  var sonSatir = ss.getLastRow() - 9;
+  var sonSatir = ss.getLastRow();
 
   for (let i = 0; i <= sonSatir; i++) {
     if (
@@ -146,9 +146,9 @@ function lefkosaDolar(islemDurumu,sube,islem,miktar){
 function lefkosaEuro(islemDurumu,sube,islem,miktar){
    var toplam = 0;
   var tarih = SpreadsheetApp.getActiveSheet().getRange("G2").getDisplayValue();
-  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D9:D").getDisplayValues();
+  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D6:D").getDisplayValues();
   var ss = SpreadsheetApp.getActiveSheet();
-  var sonSatir = ss.getLastRow() - 9;
+  var sonSatir = ss.getLastRow();
 
   for (let i = 0; i <= sonSatir; i++) {
     if (
@@ -169,9 +169,9 @@ function lefkosaEuro(islemDurumu,sube,islem,miktar){
 function lefkosaSterlin(islemDurumu,sube,islem,miktar){
    var toplam = 0;
   var tarih = SpreadsheetApp.getActiveSheet().getRange("G2").getDisplayValue();
-  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D9:D").getDisplayValues();
+  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D6:D").getDisplayValues();
   var ss = SpreadsheetApp.getActiveSheet();
-  var sonSatir = ss.getLastRow() - 9;
+  var sonSatir = ss.getLastRow();
 
   for (let i = 0; i <= sonSatir; i++) {
     if (
@@ -192,9 +192,9 @@ function lefkosaSterlin(islemDurumu,sube,islem,miktar){
 function toplamUSDT(islemDurumu,islem,miktar){
   var toplam = 0 ;
   var tarih = SpreadsheetApp.getActiveSheet().getRange("G2").getDisplayValue();
-  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D9:D").getDisplayValues();
+  var islemTarih = SpreadsheetApp.getActiveSheet().getRange("D6:D").getDisplayValues();
   var ss = SpreadsheetApp.getActiveSheet();
-  var sonSatir = ss.getLastRow() - 9;
+  var sonSatir = ss.getLastRow();
 
   for(let i = 0 ; i <= sonSatir;i++){
     if(islemDurumu[i][0] && tarih === islemTarih[i][0]){
@@ -375,9 +375,7 @@ function lefkosaSterlinHesapla(islemDurumu,sube,islem,kur,miktar){
   return toplam;
 }
 
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-// Kasa Kontrol //
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+/* Kasa Hesaplama */
 
 function sayfaBelirle(){
   var ay = SpreadsheetApp.getActiveSpreadsheet().getSheetName();
@@ -386,17 +384,93 @@ function sayfaBelirle(){
   return ay[0];
 }
 
-//NOT: sayıları tamamen doğru buluyoruz ancak dinamik bir şekilde sol tabloya nasıl yansıtacağız?
 
-function merkezIslemToplamTl(a1Notation){
-  var sayfa = sayfaBelirle();
-  var islemTarih = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfa).getRange("G2").getDisplayValue();
-  var tarih = SpreadsheetApp.getActiveSheet().getRange(a1Notation).getDisplayValue();
+function merkezHesapla(sonDurum,kur,islemDurumu,tarihRange){
+  var sayfaAdi = sayfaBelirle();
 
-  if(islemTarih === tarih){
-    return SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfa).getRange("I3").getValue();
+  var tarih = Utilities.formatDate(tarihRange,"GMT+3","dd.MM.yyyy");
+  var islemTarih = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi).getRange("G2").getDisplayValue();
+  var girdiCiktiTarih = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi + " Girdi/Çıktı").getRange("H2").getDisplayValue();
+
+  if(tarih === islemTarih && tarih === girdiCiktiTarih){
+    kur = kur.toLowerCase();
+  var tl = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi).getRange("I3").getValue() + SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi + " Girdi/Çıktı").getRange("I3").getValue();
+  var dolar = (SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi).getRange("J3").getValue() + SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi + " Girdi/Çıktı").getRange("J3").getValue());
+  var euro = (SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi).getRange("K3").getValue() + SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi + " Girdi/Çıktı").getRange("K3").getValue());
+  var sterlin = (SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi).getRange("L3").getValue() + SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi + " Girdi/Çıktı").getRange("L3").getValue());
+  
+  if(islemDurumu){
+    switch(kur){
+    case "tl":
+      return sonDurum + (tl);
+    case "dolar":
+      return sonDurum + (dolar);
+    case "euro":
+      return sonDurum + (euro);
+    case "sterlin":
+      return sonDurum + (sterlin)
+    default:
+      break;
+  } 
+  }else {
+    return 0;
+  }
+  }else{
+    return 0;
   }
 }
 
-sayfaBelirle();
+function lefkosaHesapla(sonDurum,kur,islemDurumu,tarihRange){
+  var sayfaAdi = sayfaBelirle();
 
+  var tarih = Utilities.formatDate(tarihRange,"GMT+3","dd.MM.yyyy");
+  var islemTarih = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi).getRange("G2").getDisplayValue();
+
+  var girdiCiktiTarih = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi + " Girdi/Çıktı").getRange("H2").getDisplayValue();
+  if(tarih === islemTarih && tarih === girdiCiktiTarih){
+  kur = kur.toLowerCase();
+  var tl = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi).getRange("M3").getValue() + SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi + " Girdi/Çıktı").getRange("M3").getValue();
+  var dolar = (SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi).getRange("N3").getValue() + SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi + " Girdi/Çıktı").getRange("N3").getValue());
+  var euro = (SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi).getRange("O3").getValue() + SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi + " Girdi/Çıktı").getRange("O3").getValue());
+  var sterlin = (SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi).getRange("P3").getValue() + SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi + " Girdi/Çıktı").getRange("P3").getValue());
+  
+  if(islemDurumu){
+    switch(kur){
+    case "tl":
+      return sonDurum + (tl);
+    case "dolar":
+      return sonDurum + (dolar);
+    case "euro":
+      return sonDurum + (euro);
+    case "sterlin":
+      return sonDurum + (sterlin)
+    default:
+      break;
+  } 
+  }else{
+    return 0;
+  }
+
+}else{
+  return 0;
+}
+
+}
+
+function sonUSDTDurumuHesapla(sonUSDT,islemDurumu,tarihRange){
+  var sayfaAdi = sayfaBelirle();
+
+  var tarih = Utilities.formatDate(tarihRange,"GMT+3","dd.MM.yyyy");
+  var islemTarih = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi).getRange("G2").getDisplayValue();
+
+  if(tarih === islemTarih){
+    var islem = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sayfaAdi).getRange("Q3").getValue();
+  if(islemDurumu){
+    return sonUSDT + (islem);
+  }else{
+    return 0;
+  }
+  }else{
+    return 0;
+  }
+}
